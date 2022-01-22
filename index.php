587ec1c1ddef3553ba39de6939b909975204ad69
alanpapillu <alan.papillu@hotmail.com>
2022-01-22 19:12:40 -0300

    cambios

--- index.php
+++ index.php
@@ -42,20 +42,7 @@ $pg= "index";
 
     </main>
 
-    <footer class="container my-5">
-        <div class="row">
-            <div class="col-12 col-sm-3 text-center text-sm-start">
-                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/nelson-daniel-tarche/" target="_blank" title="Linkedin">
-                    <i class="fab fa-linkedin-in"></i></a>
-            </div>
-            <div class="col-12 col-sm-3 text-center">Sponsor <a href="http://depcsuite.com" target="_blank">DePc Suite</a></div>
-            <div class="col-12 col-sm-3 text-center"><a href="mailto:info@nelsontarche.com.ar" target="_blank">info@nelsontarche.com.ar</a></div>
-        </div>
-        <div class="bloque-whatsapp">
-            <a href="https://wa.me/542216224782"><i class="fab fa-whatsapp pb-4 pt-3 px-3"></i></a>
-        </div>
-    </footer>
+    <?php include_once "footer.php"; ?>
     
 </body>
 <script src="../Portfolio/css/bootstrap/js/bootstrap.bundle.min.js"></script>
